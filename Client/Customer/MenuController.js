a185bafe27824d4f954a9665d14b771f4a4b2daf
Dulaj <shad3s.of.m3@gmail.com>
2022-03-26 13:52:42 +0530

    added notifications to all pages

--- Client/Customer/MenuController.js
+++ Client/Customer/MenuController.js
@@ -14,60 +14,6 @@ var type = "";
 var preference = "";
 
 $(document).ready(function () {
-  if (!window.localStorage.getItem("customer")) {
-    console.log("hello");
-    var signUp = $(document.createElement("li")).addClass("ml-5 btn");
-
-    $("#nav-items").append(signUp);
-    var upLink = $(document.createElement("a"))
-      .attr("href", "customer-login.html")
-      .html("Sign Up");
-    signUp.append(upLink);
-
-    var signIn = $(document.createElement("li")).addClass("ml-5 btn bg-black");
-    var inLink = $(document.createElement("a"))
-      .attr("href", "customer-login.html")
-      .html("Sign In");
-    signIn.append(inLink);
-    $("#nav-items").append(signUp);
-    $("#nav-items").append(signIn);
-  } else {
-    var name = $(document.createElement("li"))
-      .addClass("fw-b pl-5")
-      .html(window.localStorage.getItem("name"));
-
-    var avatarContainer = $(document.createElement("li")).addClass("pl-5");
-    var link = $(document.createElement("a")).attr(
-      "href",
-      "customer-profile.html"
-    );
-    var avatar = $(document.createElement("img"))
-      .addClass("icon-2")
-      .attr("src", window.localStorage.getItem("avatar"));
-    var badge = $(document.createElement("span"))
-      .addClass("icon-button__badge notificationCount")
-      .html(2);
-    var bell = $(document.createElement("span"))
-      .addClass("material-icons")
-      .html("notifications");
-
-    var notification = $(document.createElement("button")).addClass(
-      "icon-button "
-    );
-
-    notification.append(bell);
-    notification.append(badge);
-
-    // getNotifications();
-    // getInformation();
-
-    link.append(avatar);
-    avatarContainer.append(link);
-    $("#nav-items").append(notification);
-    $("#nav-items").append(avatarContainer);
-    $("#nav-items").append(name);
-  }
-
   window.setInterval(function () {
     var today = new Date();
     var lunch = "12:00:00";
